8e8aa8464a53d172c07d275abd5388e0a626a57b
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-04-14 23:18:54 +0500

    Design & Develop Calculator App

---
+++ Calculator App/app.js
@@ -0,0 +1,15 @@
+function getNum(nm){
+	var result = document.getElementById("result");
+	result.value += nm;
+}
+
+
+function clearResult(){
+	var result = document.getElementById("result")
+	result.value = "";
+}
+
+function getResult(){
+	var result = document.getElementById("result")
+	result.value = eval(result.value)
+}
