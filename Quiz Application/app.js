11eab5004b435e57fe0df0654e59b3a937bcce94
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-04-25 23:55:23 +0500

    Update app.js

---
+++ Quiz Application/app.js
@@ -0,0 +1,53 @@
+var questionsArray = [
+    {
+        question: "Full Form of RAM is?",
+        answer: "random access memory",
+        options: [
+            "random access memory",
+            "random actual memory",
+            "right access memory",
+            "none of the above",
+        ]
+    },
+    {
+        question: "Full Form of CPU is?",
+        answer: "central processing unit",
+        options: [
+            "central program unit",
+            "central processing unit",
+            "control panel unit",
+            "none of the above",
+        ]
+    },
+    {
+        question: "Full Form of E-MAIL is?",
+        answer: "electronic mail",
+        options: [
+            "electric mail",
+            "easy mail",
+            "electronic mail",
+            "none of the above",
+        ]
+    },
+    {
+        question: "Full Form of LCD is?",
+        answer: "liquid crystal display",
+        options: [
+            "liquid crystal display",
+            "light crystal display",
+            "liquid cold display",
+            "none of the above",
+        ]
+    },
+    {
+        question: "Full Form of SEO is?",
+        answer: "none of the above",
+        options: [
+            "safe engine optimization",
+            "secure engine optimization",
+            "secret engine optimization",
+            "none of the above",
+        ]
+    },
+];
+
