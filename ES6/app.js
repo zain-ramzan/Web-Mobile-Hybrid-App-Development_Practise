fa3ac0d2a1658dd5ba96d19ac26add456c9e68bb
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-06-17 20:19:58 +0500

    ES-06

---
+++ ES6/app.js
@@ -0,0 +1,5 @@
+//------------------------- Template Literals-------------------------
+let fname = prompt("Enter your name...","Zain Ramzan")
+console.log(fname);
+document.write(`My name is ${fname}`)
+
