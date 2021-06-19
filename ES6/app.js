2ba17bcd4f9c8ca2351cbbf24b4ec21f31d3b57b
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-06-19 22:57:25 +0500

    Templete Literals

--- ES6/app.js
+++ ES6/app.js
@@ -1,5 +1,31 @@
 //------------------------- Template Literals-------------------------
-let fname = prompt("Enter your name...","Zain Ramzan")
-console.log(fname);
-document.write(`My name is ${fname}`)
+let fname = "Zain";
+let lname = "Ramzan"
+document.write(`My name is ${fname} ${lname} <br> <br> `)
 
+
+// firebase.database().ref(`school/class/section/rollno/name`)
+
+
+
+let data = {
+    "id": 1,
+    "name": "Bulbasaur",
+    "base_experience": 64,
+    "height": 7,
+    "is_default": true,
+    "order": 1,
+    "weight": 69
+};
+console.log(data.name);
+
+document.write(`<h3> The name of seller is ${data.name}, his experience is ${data.base_experience}. His weight is ${data.weight}.</h3>`)
+
+let getData =(string,value)=>{
+    console.log(string,value);
+}
+let text = 'developer';
+getData `zain  ${text} `
+
+
+//------------------------- Template Literals-------------------------
