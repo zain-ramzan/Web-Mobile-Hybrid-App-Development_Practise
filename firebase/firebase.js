7b650ba5a91a2535aec867446fb5d94664e3b715
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-06-23 10:32:34 +0500

    Develop Firebase Auth

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,17 +1,35 @@
-
-
-var nm = document.getElementById('username');
+var email = document.getElementById('email');
 var pass = document.getElementById("password")
-
-
-
-function getData(){
-    console.log(nm.value);
-    console.log(pass.value);
-
-
-
-    
-    nm.value = ""
+function signup(){
+    firebase.auth().createUserWithEmailAndPassword(email.value, pass.value)
+    .then((result) => {
+        alert("User Sign Up Successfully!")
+        console.log("Result Message ==>",result);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+    email.value = ""
     pass.value = ""
+}
+
+
+//FOR LOGIN USER
+var em = document.getElementById('l-email');
+var pas = document.getElementById("l-password")
+function login(){
+    firebase.auth().signInWithEmailAndPassword(em.value, pas.value)
+    .then((result) => {
+        alert("User Login Up Successfully!")
+        console.log("Result Message ==>",result);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+    em.value = ""
+    pas.value = ""
 }
