15d3771743813b2ef92bd005da8baab535be3ea2
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-06-23 23:49:44 +0500

    create home pagae

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,8 +3,9 @@ var pass = document.getElementById("password")
 function signup(){
     firebase.auth().createUserWithEmailAndPassword(email.value, pass.value)
     .then((result) => {
-        alert("User Sign Up Successfully!")
         console.log("Result Message ==>",result);
+        window.location = "home.html"
+
     })
     .catch((error) => {
       var errorCode = error.code;
@@ -22,8 +23,8 @@ var pas = document.getElementById("l-password")
 function login(){
     firebase.auth().signInWithEmailAndPassword(em.value, pas.value)
     .then((result) => {
-        alert("User Login Up Successfully!")
         console.log("Result Message ==>",result);
+        window.location = "home.html";
     })
     .catch((error) => {
       var errorCode = error.code;
@@ -33,3 +34,33 @@ function login(){
     em.value = ""
     pas.value = ""
 }
+
+var card = document.getElementById("container");
+
+card.style.backgroundColor = "goldenrod"
+card.style.color = "white"
+card.style.margin = "30px auto"
+card.style.borderRadius = "10px"
+card.style.width = "50%"
+// card.style.height = "500px"
+var main_content = document.createElement("p")
+var main_text = document.createElement("h1")
+var name_text = document.createTextNode("Name:")
+main_text.appendChild(name_text)
+
+var main_email = document.createElement("h1")
+var email_text = document.createTextNode("Email:")
+main_email.appendChild(email_text)
+
+
+main_content.style.padding = "30px 50px"
+
+main_content.appendChild(main_email)
+main_content.appendChild(main_text)
+
+main_content.style.display = "display-block"
+// main_content.style.margin = "100px 50px"
+
+container.appendChild(main_content)
+
+console.log(main_content);
