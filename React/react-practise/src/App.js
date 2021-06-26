c3e5b5f4185e1dac90130399c0e993d27417e95b
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-06-26 10:13:10 +0500

    Learn Maping

--- React/react-practise/src/App.js
+++ React/react-practise/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 
 function App() {
-  let firstName = "Zain";
-  let lastName = "Ramzan";
+  let todos = [{name:"Zain"},{name:"Saad"}]
   return (
     <div>
       <h1>React Practise</h1>
     	<h4>-----------------------------------------------------</h4>
-      
+    	<h4>Learn Maping</h4>
+      <ul>
+        {todos.map((item,index)=>{
+          return(
+            <li key={index}>{item.name}</li>
+          )
+        })}
+      </ul>
+
+
+
     </div>
   );
 }
