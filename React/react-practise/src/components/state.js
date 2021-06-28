3b9a2668f0b549af511bbf495d1fc901d79c1414
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-06-28 06:10:58 +0500

    Update the username

--- React/react-practise/src/components/state.js
+++ React/react-practise/src/components/state.js
@@ -11,16 +11,20 @@ class Learn extends React.Component {
             institute : "PIAIC"
         }
     }   
+    set_name = () => {
+        this.setState({
+            name: "Hamza"
+        })
+    }
+
 
 
     //Create Function inside the constructor
         //Don't use Function keyword - Simply use function name e.g set_name
-    set_name = () => {
+    get_name = () => {
         console.log(this.state.name);
-        console.log(this.state.batch);
-        console.log(this.state.institute);
-        console.log("-------------------");
     };
+    set_
 
 
     render(){
@@ -30,7 +34,8 @@ class Learn extends React.Component {
             <h4>My name is {this.state.name}</h4>
             <h4>My Batch is: {this.state.batch}</h4>
             <h4>My Institute name is:{this.state.institute}</h4>
-            <button onClick={this.set_name}>Get Student Detaile</button>
+            <button onClick={this.set_name}>Set Student Detaile</button> <br/>
+            <button onClick={this.get_name}>Get Student Detaile</button>
             </div>
         )
     } 
