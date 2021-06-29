f39cec1e4049821ba9b57bda228512386fb78d69
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-06-29 12:48:01 +0500

    learn setState

--- React/react-practise/src/components/state.js
+++ React/react-practise/src/components/state.js
@@ -6,14 +6,18 @@ class Learn extends React.Component {
     constructor(){
         super()
         this.state = {
-            name : "Hamza Ramzan",
-            batch : "35",
-            institute : "PIAIC"
+            name : "Zain Ramzan",
+            email: "zain@gmail.com"
         }
     }   
     set_name = () => {
         this.setState({
             name: this.state.value
+        })
+    }
+    getValue = (e) =>{
+        this.setState({
+            [e.target.name]: e.target.value
         })
     }
 
@@ -30,15 +34,40 @@ class Learn extends React.Component {
     render(){
         return(
             <div>
-            <h2>State</h2>
+            {/* 
             <h4>My name is  {this.state.name}</h4>
             <h4>My Batch is: {this.state.batch}</h4>
             <h4>My Institute name is:{this.state.institute}</h4>
-            <input type='text' placeholder="Real time input..." onChange={(e)=>this.setState({name: e.target.value})} />
+            <input type='text'onChange={(e)=>this.setState({name: e.target.value})} />
             <h3>-------</h3>
-            <input type='text' placeholder="Enter you Name..." onChange={(e)=>this.setState({value: e.target.value})} /><br/><br/>
+            <input type='text' onChange={(e)=>this.setState({value: e.target.value})} /><br/><br/>
             <button onClick={this.set_name}>Set Student Name</button> <br/>
-            <button onClick={this.get_name}>Get Student</button>
+            <button onClick={this.get_name}>Get Student</button> */}
+
+
+
+            <h2>State</h2>
+            <h3>Student Name: {this.state.name}</h3>
+            <h3>Student Email: {this.state.email}</h3>
+            <input 
+                name="name" 
+                text="text" 
+                placeholder="Enter your Name..." 
+                onChange={(e)=>this.getValue(e)} 
+                />
+            <br/>
+            <br/>
+            <input 
+                name="email"  
+                text="text" 
+                placeholder="Enter your Email..." 
+                onChange={(e)=>this.getValue(e)} 
+            />
+
+
+
+
+
             </div>
         )
     } 
