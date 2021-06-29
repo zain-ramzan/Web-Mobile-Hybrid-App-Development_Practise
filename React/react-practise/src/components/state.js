e3e135cf854c4727d907bf57fcb4c806f9a97b9b
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-06-29 09:43:21 +0500

    learn setstate

--- React/react-practise/src/components/state.js
+++ React/react-practise/src/components/state.js
@@ -36,7 +36,7 @@ class Learn extends React.Component {
             <h4>My Institute name is:{this.state.institute}</h4>
             <input type='text' placeholder="Enter your name" onChange={(e)=>this.setState({name: e.target.value})} /><br /><br/>
             <button onClick={this.set_name}>Set Student Name</button> <br/>
-            <button onClick={this.get_name}>Get Student Detaile</button>
+            <button onClick={this.get_name}>Get StudentDetaile</button>
             </div>
         )
     } 
