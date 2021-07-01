a24166c7f61457360ee646eff1c1866b6373756e
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-07-01 11:25:52 +0500

    Learn Todos List

--- React/react-practise/src/components/todos.jsx
+++ React/react-practise/src/components/todos.jsx
@@ -4,17 +4,34 @@ class Todos extends React.Component {
     constructor() {
         super()
         this.state = {
-            todos: ["Zain", "Hamza", "Saad"]
+            todos: ["Zain"],
+            value: ''
         }
+    }
+    get_todos = () => {
+        this.setState({
+            todos: [...this.state.todos, this.state.value],
+            value: ""
+        })
     }
 
     render() {
+        let { todos, value } = this.state;
         return (
             <div>
                 <h2>Todo List</h2>
-                <input type="text" placeholder="Enter the Value..." onChange={(e)=>console.log(e.target.value)} />
+                <input 
+                    type="text" 
+                    placeholder="Enter the Value..." 
+                    onChange={(e) => this.setState({ value: e.target.value })} 
+                    value={value} 
+                    /> 
+                    
+
+                <button 
+                onClick={this.get_todos}>Add Todos</button>
                 <ul>
-                    {this.state.todos.map((v, i) => {
+                    {todos.map((v, i) => {
                         return <li>{i} - {v}</li>
                     }
                     )}
