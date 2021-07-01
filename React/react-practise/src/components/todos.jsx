645d9fb214170aa81147e2fd99b20c7c91b222c4
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-07-01 12:46:54 +0500

    Add Edit Option in the Todos Application using React

--- React/react-practise/src/components/todos.jsx
+++ React/react-practise/src/components/todos.jsx
@@ -4,18 +4,37 @@ class Todos extends React.Component {
     constructor() {
         super()
         this.state = {
-            todos: ["Zain"],
+            todos: [{title: "Zain Ramzan",edit: false}],
             value: ''
         }
     }
     get_todos = () => {
+        let text = {title: this.state.value}
         this.setState({
-            todos: [...this.state.todos, this.state.value],
+            todos: [...this.state.todos, text],
             value: ""
         })
     }
     delete_list = (i) =>{
         this.state.todos.splice(i,1);
+        this.setState({
+            todos: this.state.todos
+        })
+    }
+    edit_todo =(i)=>{
+        this.state.todos[i].edit = true
+        this.setState({
+            todos: this.state.todos
+        })
+    }
+    handlechange=(e,i)=>{
+        this.state.todos[i].title = e.target.value
+        this.setState({
+            todos: this.state.todos
+        })
+    }
+    update_todo=(i)=>{
+        this.state.todos[i].edit = false
         this.setState({
             todos: this.state.todos
         })
@@ -32,11 +51,18 @@ class Todos extends React.Component {
                     onChange={(e) => this.setState({ value: e.target.value })} 
                     value={value} 
                     />
-                <button 
-                onClick={this.get_todos}>Add Todos</button>
+                <button onClick={this.get_todos}>Add Todos</button>
                 <ul>
                     {todos.map((v, i) => {
-                        return <li key={i}>{i+1} - {v} <button onClick={()=>this.delete_list(i)}>Delete</button> </li>
+                        return <li key={i}>{i+1} - 
+                        {v.edit ? <input type="text" value={v.title} onChange={(e)=>this.handlechange(e,i)}/> : v.title} 
+                        {v.edit ? 
+                        <button onClick={()=>this.update_todo(i)}>Update</button> 
+                        : 
+                        <button onClick={()=>this.edit_todo(i)}>Edit</button>
+                        }
+                        <button onClick={()=>this.delete_list(i)}>Delete</button>
+                         </li>
                     }
                     )}
                 </ul>
