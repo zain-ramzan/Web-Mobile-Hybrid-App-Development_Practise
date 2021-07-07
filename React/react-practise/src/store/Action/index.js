2ef3fbe37c644be8e06bca3d3527b1b38817320f
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-07-07 22:09:27 +0500

    Get & Update the state using Redux

--- React/react-practise/src/store/Action/index.js
+++ React/react-practise/src/store/Action/index.js
@@ -1,6 +1,7 @@
-const set_data =()=>{
-    return (dispatch)=>{
-console.log("hloo");
+const set_data = () => {
+    return (dispatch) => {
+        console.log("hloo");
+        dispatch({type: "Hello World! React Js"})
     }
 }
 
