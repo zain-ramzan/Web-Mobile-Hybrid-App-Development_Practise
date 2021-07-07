1abb82278c8250ba7b9887cf88bbcf36aab6d75a
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-07-07 18:29:48 +0500

    React Redux is working fine

--- React/react-practise/src/App.js
+++ React/react-practise/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+
 class App extends React.Component {
   render() {
+    console.log("React-Redux Data ===>", this.props);
     return (
       <div>
         <h1>React Js Practise</h1>
-        <button onClick={()=>this.props.history.push("/todo_application")}>Todo App</button>
-        <button onClick={()=>this.props.history.push("/maping")}>Maping</button>
-        <button onClick={()=>this.props.history.push("/state")}>State</button>
-        <button onClick={()=>this.props.history.push("/props")}>Props</button>
+        <button onClick={() => this.props.history.push("/todo_application")}>Todo App</button>
+        <button onClick={() => this.props.history.push("/maping")}>Maping</button>
+        <button onClick={() => this.props.history.push("/state")}>State</button>
+        <button onClick={() => this.props.history.push("/props")}>Props</button>
 
-         {/*<Maping page="State Pages using Props" /> 
+        {/*<Maping page="State Pages using Props" /> 
         <Learn page="State Pages using Props" />
         <Props message="This Props is from parent-to-child" />
         <Todos /> */}
@@ -18,4 +21,8 @@ class App extends React.Component {
     );
   }
 }
-export default App;
+
+const mapStateToProps = (state) => ({
+  name: state.name
+})
+export default connect(mapStateToProps, null)(App);
